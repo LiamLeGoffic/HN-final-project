1a9a2c26c3dbc670100d6a59499f276ea03da989
LiamLeGoffic <liamlegoffic@yahoo.fr>
2024-12-05 16:16:58 +0100

    second commit

--- FinalProject/src/main/java/com/HNformation/FinalProject/entity/User.java
+++ FinalProject/src/main/java/com/HNformation/FinalProject/entity/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.util.Objects;
 
@@ -29,7 +31,8 @@ public class User {
     private String email;
 
     @ManyToOne
-    @JoinColumn(name = "user_type_id")
+    @JoinColumn(name = "user_type_id", nullable = true)
+    @OnDelete(action = OnDeleteAction.SET_NULL)
     private UserType userType;
 
     public User(String lastName, String firstName, String email, UserType userType) {
